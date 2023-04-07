dba3d462310efcc4fe3aa4cc2bb08f1d187685a2
HanshengGUO <hsguo@aol.com>
2023-04-07 15:12:44 +0200

    Project Configuration

---
+++ src/main/java/com/hsguo/codepedia/CodepediaApplication.java
@@ -0,0 +1,13 @@
+package com.hsguo.codepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodepediaApplication.class, args);
+    }
+
+}
