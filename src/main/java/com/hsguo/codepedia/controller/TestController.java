05f326291637c9387f2eb8781dd567853ba23d9f
HanshengGUO <hsguo@aol.com>
2023-04-07 20:50:08 +0200

    Add api testing using idea http client

--- src/main/java/com/hsguo/codepedia/controller/TestController.java
+++ src/main/java/com/hsguo/codepedia/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.hsguo.codepedia.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -16,8 +17,13 @@ public class TestController {
      * 使用错误的方法会返回错误405，Method Not Allowed
      * @return
      */
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
        return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post " + name;
     }
 }
