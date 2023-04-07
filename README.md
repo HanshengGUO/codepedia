af44b3217c4b1b679b862d3cbf9f03b37894ac0c
HanshengGUO <hsguo@aol.com>
2023-04-07 20:23:07 +0200

    Update Start Information Printing

---
+++ README.md
@@ -0,0 +1,1 @@
+# Codepedia: An online knowledge base for developers
--- src/main/java/com/hsguo/codepedia/CodepediaApplication.java
+++ src/main/java/com/hsguo/codepedia/CodepediaApplication.java
@@ -1,13 +1,20 @@
 package com.hsguo.codepedia;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CodepediaApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(CodepediaApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(CodepediaApplication.class, args);
+        SpringApplication app = new SpringApplication(CodepediaApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Successfully started!");
+        // 读取配置项来得到端口号
+        LOG.info("address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
