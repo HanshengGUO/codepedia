822d7416f569cbd30082063d6586e597f57b6752
HanshengGUO <hsguo@aol.com>
2023-04-09 20:54:40 +0200

    Test demo request

---
+++ src/main/java/com/hsguo/codepedia/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.hsguo.codepedia.controller;
+
+import com.hsguo.codepedia.domain.Demo;
+import com.hsguo.codepedia.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/*
+RestController 返回一个字符串
+Controller 返回一个页面
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @Resource
+    private DemoService demoService;
+
+    @GetMapping("/list")
+    public List<Demo> list(){
+        return demoService.list();
+    }
+}
