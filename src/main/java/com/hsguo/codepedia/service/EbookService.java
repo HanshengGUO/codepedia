de133b76ee5b9a0dafee0584e83b728be56ef671
HanshengGUO <hsguo@aol.com>
2023-04-27 11:26:55 +0200

    Use PageHelper to do the paging

--- src/main/java/com/hsguo/codepedia/service/EbookService.java
+++ src/main/java/com/hsguo/codepedia/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.hsguo.codepedia.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.hsguo.codepedia.domain.Ebook;
 import com.hsguo.codepedia.domain.EbookExample;
 import com.hsguo.codepedia.mapper.EbookMapper;
 import com.hsguo.codepedia.req.EbookReq;
 import com.hsguo.codepedia.resp.EbookResp;
 import com.hsguo.codepedia.utils.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,18 +18,27 @@ import java.util.List;
 
 @Service
 public class EbookService {
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     // 也可以用Autowired注解
     // Resource是JDK自带的
     // Autowired是来自Spring
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<EbookResp> list(EbookReq req){
+    public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName()))
             criteria.andNameLike("%" + req.getName() + "%");
+        // 只对第一次查询的sql有效，所以尽量和查询语句放在一起
+        // 分页的基础数据四条，返回给前端计算
+        PageHelper.startPage(1, 3);
         List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebooksList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
         // null 相当于 new EbookExample()
         List<EbookResp> ebooksRespList = CopyUtil.copyList(ebooksList, EbookResp.class);
 //        List<EbookResp> ebooksRespList = new ArrayList<>();
