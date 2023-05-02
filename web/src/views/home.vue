250137ac37f896ac4b8043430cdd122e132bcaf4
HanshengGUO <hsguo@aol.com>
2023-05-02 20:58:00 +0200

    Add category menu on home page

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -7,46 +7,24 @@
   <a-layout style="padding: 24px 0; background: #fff">
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
           mode="inline"
-          style="height: 100%"
+          :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-                <span>
-                  <user-outlined/>
-                  subnav 1
-                </span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined/>
+            <span>Welcome</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined/>{{ item.name }}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-                <span>
-                  <laptop-outlined/>
-                  subnav 2
-                </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-                <span>
-                  <notification-outlined/>
-                  subnav 3
-                </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined/>
+            <span>{{ child.name }}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -77,8 +55,10 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, reactive, ref} from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
+import {message} from 'ant-design-vue';
+import {Tool} from "@/utils/tool";
 
 // const listData: any = [];
 //
@@ -99,9 +79,35 @@ export default defineComponent({
   setup() {
     console.log("setup invoked");
     const ebooks = ref();
-    const ebooks1 = reactive({books: []});
+    // const ebooks1 = reactive({books: []});
+
+    const level1 = ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = () => {
+      console.log("menu click")
+    };
     // 生命周期Hook函数
     onMounted(() => {
+      handleQueryCategory();
       axios.get("/ebook/list", {
         params: {
           page: 1,
@@ -128,6 +134,8 @@ export default defineComponent({
         {type: 'LikeOutlined', text: '156'},
         {type: 'MessageOutlined', text: '2'},
       ],
+      level1,
+      handleClick,
     }
   }
 });
