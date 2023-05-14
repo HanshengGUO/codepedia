3fbbd227b9099307bf7719e220331cbdf1db3e85
HanshengGUO <hsguo@aol.com>
2023-05-14 13:45:11 +0200

    Design User table and generate persistent layer code

---
+++ src/main/java/com/hsguo/codepedia/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.hsguo.codepedia.mapper;
+
+import com.hsguo.codepedia.domain.User;
+import com.hsguo.codepedia.domain.UserExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+}
