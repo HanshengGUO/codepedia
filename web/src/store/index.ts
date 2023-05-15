f6993a405d3b6b9fdb93f91f155d63bd97166715
HanshengGUO <hsguo@aol.com>
2023-05-15 16:29:27 +0200

    Solve bugs: login information retains after page refreshing

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,12 +1,17 @@
 import {createStore} from 'vuex'
 
+declare let SessionStorage: any;
+const USER = "USER";
+
 const store = createStore({
     state: {
-        user: {}
+        // 加载，加载不到则返回空对象
+        user: SessionStorage.get(USER) || {}
     },
     mutations: {
         setUser(state, user) {
             state.user = user;
+            SessionStorage.set(USER, user);
         }
     },
     actions: {},
